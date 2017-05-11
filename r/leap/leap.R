eb4c1284ccf748c710d9f0d07c8d458db69f18b6
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-05-11 18:58:49 +0200

    iterate verbosity

--- r/leap/leap.R
+++ r/leap/leap.R
@@ -1,8 +1,3 @@
 leap <- function(year) {
-  
-  if (year %% 400 == 0 | year %% 4 == 0 & year %% 100 != 0)
-    return(TRUE)
-  else
-    return(FALSE)
-  
+  year %% 400 == 0 | year %% 4 == 0 & year %% 100 != 0
 }
