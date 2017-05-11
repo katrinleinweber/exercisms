d68263983dd361829f7b761437c4be595d59bddd
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-05-11 18:58:21 +0200

    iterate pattern

--- r/leap/leap.R
+++ r/leap/leap.R
@@ -1,12 +1,8 @@
 leap <- function(year) {
   
-  if (year %% 4 == 0) {
-    
-    if (year %% 100 != 0)  
-      return(TRUE)
-    else if (year %% 400 == 0)  # together with `!=` above means "unless also"
-        return(TRUE)
-  }
+  if (year %% 400 == 0 | year %% 4 == 0 & year %% 100 != 0)
+    return(TRUE)
+  else
+    return(FALSE)
   
-  return(FALSE)
 }
