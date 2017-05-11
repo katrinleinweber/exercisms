bdaa25419f11d5caf08d7c413689149d0bc671bc
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-05-11 18:41:19 +0200

    iterate flow

--- r/leap/leap.R
+++ r/leap/leap.R
@@ -2,14 +2,10 @@ leap <- function(year) {
   
   if (year %% 4 == 0) {
     
-    if (year %% 100 == 0) {
-      
-      if (year %% 400 == 0)
+    if (year %% 100 != 0)  
+      return(TRUE)
+    else if (year %% 400 == 0)  # together with `!=` above means "unless also"
         return(TRUE)
-      
-      return(FALSE)
-    }
-    return(TRUE)
   }
   
   return(FALSE)
