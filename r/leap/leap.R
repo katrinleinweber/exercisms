944356e4ab722fd433b5835d98d255f7e3080f6f
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-05-11 18:12:30 +0200

    solve

--- r/leap/leap.R
+++ r/leap/leap.R
@@ -1,3 +1,18 @@
 leap <- function(year) {
   
+  if (year %% 4 == 0) {
+    
+    if (year %% 100 == 0) {
+      
+      if (year %% 400 == 0) {
+        return(TRUE)
+      } 
+      
+      return(FALSE)
+    }
+    
+    return(TRUE)
+  }
+  
+  return(FALSE)
 }
