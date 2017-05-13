64315423152b3a160d128d1a390be0c9bb040f7f
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-05-13 11:56:13 +0200

    solve almost
    http://exercism.io/submissions/71fa414b1c6f4695b317286cc91488b5

--- r/bob/bob.R
+++ r/bob/bob.R
@@ -1,3 +1,28 @@
-bob <- function() {
+library(stringi)
 
+bob <- function(input) {
+  
+  # remove white-space
+  input <- stri_replace_all_regex(input, "\\s+", "")
+  
+  dplyr::case_when(
+    
+    # yelling
+    stri_count_regex(input, "[[:upper:]]") > 
+      stri_count_regex(input, "[[:lower:]]") ~ 
+      paste("Whoa, chill out!"),
+    
+    # question
+    stri_endswith(input, fixed = "?") ~ 
+      paste("Sure."),
+    
+    # not saying anything
+    input == "" | 
+      stri_count_regex(input, "[[:punct:]]") > 
+      stri_count_regex(input, "[[:alnum:]]") ~ 
+      paste("Fine. Be that way!"),
+    
+    # everything else
+    TRUE ~ paste("Whatever.")
+  )
 }
