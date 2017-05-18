3f3f5de87342b9f8d43e10643e99c331716c5b23
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-05-18 20:50:47 +0200

    iterate: remove final, obsolete predicate

--- r/hamming/hamming.R
+++ r/hamming/hamming.R
@@ -4,16 +4,11 @@ hamming <- function(strand1,strand2) {
   
   if (identical(strand1, strand2))
     return(0)
-  
-  if (nchar(strand1) != nchar(strand2))
+  else if (nchar(strand1) != nchar(strand2))
     stop("different lengths!")
-  
-  # detect base mistmatches, adding them up
-  if (nchar(strand1) == nchar(strand2)) {
-    
+  else {
     bases1 <- unlist(strsplit(strand1, ""))
     bases2 <- unlist(strsplit(strand2, ""))
-    
     purrr::map2_lgl(bases1, bases2, stringi::stri_cmp_neq) %>%
       sum() %>%
       return()
