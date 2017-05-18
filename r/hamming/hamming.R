5f9fd0104a97a1b26f7aef49fad184dbb2fd5b8e
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-05-18 17:22:09 +0200

    Calculate Hamming way… Erm: distance!
    http://exercism.io/submissions/ad4a4e1cb8144567b0a255b503179621

--- r/hamming/hamming.R
+++ r/hamming/hamming.R
@@ -1,4 +1,21 @@
-# This is a stub function to take two strings
-# and calculate the hamming distance
+library(magrittr)
+
 hamming <- function(strand1,strand2) {
+  
+  if (identical(strand1, strand2))
+    return(0)
+  
+  if (nchar(strand1) != nchar(strand2))
+    stop("different lengths!")
+  
+  # detect base mistmatches, adding them up
+  if (nchar(strand1) == nchar(strand2)) {
+    
+    bases1 <- unlist(strsplit(strand1, ""))
+    bases2 <- unlist(strsplit(strand2, ""))
+    
+    purrr::map2_lgl(bases1, bases2, stringi::stri_cmp_neq) %>%
+      sum() %>%
+      return()
+  }
 }
