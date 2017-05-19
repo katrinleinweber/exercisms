9bc613c3b78f5a69c76438c3a20a4eef0e187616
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-05-19 20:15:43 +0200

    solve RNA transcription with `case_when`
    http://exercism.io/submissions/579ffc0db59043cc89489fd6da176472

--- r/rna-transcription/rna-transcription.R
+++ r/rna-transcription/rna-transcription.R
@@ -1,3 +1,22 @@
 to_rna <- function(dna) {
   
+  # handle invalid sequences
+  if (stringi::stri_detect_regex(dna, "[^GATC]"))
+    stop("That's not a valid nucleobase!")
+  
+  # return complement 
+  transcribe <- function(base) {
+    dplyr::case_when(
+      base == "G" ~ "C",
+      base == "A" ~ "U",
+      base == "T" ~ "A",
+      base == "C" ~ "G"
+    )
+  }
+  
+  # swap bases 1-by-1, then concatenate to sequence
+  strsplit(dna, "") %>%
+    unlist() %>%
+    lapply(transcribe) %>%
+    paste(collapse = "")
 }
