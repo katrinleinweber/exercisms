bf556aaac97069610ab870816df054b53b15295b
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-05-20 13:31:36 +0200

    calculate grains on chess board
    http://exercism.io/submissions/9463be5d2c86423e8dd866eaa40e3f0a

--- r/grains/grains.R
+++ r/grains/grains.R
@@ -1,5 +1,14 @@
+# chess board min & max
+a1 <- 1
+g8 <- 64
+
 square <- function(n) {
+  if (dplyr::between(n, a1, g8))
+    2 ^ (n - 1)
+  else
+    stop("That's not a proper chess board!")
 }
 
 total <- function() {
+  sum(2 ^ ((a1:g8) - 1))
 }
