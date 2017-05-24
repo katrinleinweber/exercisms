4602581a962a1ce893ed1d02ef293efec7f77710
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-05-24 22:08:14 +0200

    solve naively by regex-ing anagram in both cases &…
    purging false positives later
    
    http://exercism.io/submissions/773166c19022486c88d7e2a24ceed95b

--- r/anagram/anagram.R
+++ r/anagram/anagram.R
@@ -1,3 +1,38 @@
-anagram <- function() {
+library(magrittr)
+library(stringi)
 
+anagram <- function(subject, candidates) {
+
+  options(encoding = "UTF-8")
+  
+  # buffer
+  anagrams <- c()
+  
+  if (length(candidates) <= 1)
+    return(anagrams)
+  
+  # prepare comparison
+  candidates_lengths <- stri_length(candidates)
+  
+  for (x in 1:range(length(candidates))) {
+    
+    if (candidates_lengths[x] == stri_length(subject)) {
+      
+      candidates[x] %>%
+        stri_extract_all_regex(pattern = paste0("[", tolower(subject), toupper(subject), "]")) %>%
+        unlist() %>% 
+        paste(collapse = "") ->
+        anagram
+      
+      # discard anagrams that are not different from subject
+      if (identical(anagram, subject) | identical(tolower(anagram), tolower(subject)))
+          anagram <- c()
+         
+      # store true anagrams in buffer 
+      if (identical(stri_length(anagram), stri_length(subject)))
+        anagrams <- append(anagrams, anagram)
+    }
+  }
+  
+  return(anagrams)
 }
