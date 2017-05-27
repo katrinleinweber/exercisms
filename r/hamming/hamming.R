d2c453688bcf7e3f6c32915a1a199029d866a450
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-05-27 19:16:51 +0200

    iterate: anonymise mistmatch detection function

--- r/hamming/hamming.R
+++ r/hamming/hamming.R
@@ -4,7 +4,14 @@ hamming <- function(strand1,strand2) {
   
   dplyr::case_when(
     identical(strand1, strand2) ~ as.integer(0),
-    nchar(strand1) == nchar(strand2) ~ distance(strand1, strand2)
+    nchar(strand1) == nchar(strand2) ~
+      
+      # detect base mistmatches, adding them up
+      purrr::map2_lgl(split(strand1),
+                      split(strand2),
+                      stringi::stri_cmp_neq
+                      ) %>%
+      sum()
   )
   
   # omitting explicit stop("Different lengths!") doesn't cause last test to fail
@@ -12,12 +19,4 @@ hamming <- function(strand1,strand2) {
 
 split <- function(strand) {
   strand %>% strsplit("") %>% unlist()
-}
-
-distance <- function(strand1, strand2) {
-  
-    # detect base mistmatches, adding them up
-    purrr::map2_lgl(split(strand1), split(strand2), stringi::stri_cmp_neq) %>%
-      sum() %>%
-      return()
 }
