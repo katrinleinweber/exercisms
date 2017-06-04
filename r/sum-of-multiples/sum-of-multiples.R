3f0c3ebc147df3c7aba782f3668d45aeb65952cb
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-06-04 14:30:33 +0200

    iterate on buffer: exclude duplicates right away
    http://exercism.io/submissions/ef2a5aee004e4d4cbebc81d56a8a6812

--- r/sum-of-multiples/sum-of-multiples.R
+++ r/sum-of-multiples/sum-of-multiples.R
@@ -3,14 +3,17 @@ sum_of_multiples <- function(factors, limit) {
   # create vector of candidate integers
   candidates <- seq(limit - 1)
   
-  # narrow candidates down to factors & multiples
+  # narrow candidates down to factors & unique multiples
   multiples <- c()
   for (f in factors)
     for (c in candidates)
-      if (c %% f == 0)
+      if (c %% f == 0 & !(c %in% multiples))
         multiples <- append(multiples, c)
+  # [ ] recursive append?
+  # [ ] use interaction()?
+  # [ ] purrr::map or ::walk on differently long vectors?
   
   # sum those up
-  sum(unique(multiples))
+  sum(multiples)
   
 }
