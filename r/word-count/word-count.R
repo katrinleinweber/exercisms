135b7b905f02fcd57088627eaf5e6254d8e96c29
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-06-04 15:17:23 +0200

    solve naively with for-loop
    http://exercism.io/submissions/fd1ea08a8e034ce7955bd8a49eebe6bd

--- r/word-count/word-count.R
+++ r/word-count/word-count.R
@@ -1,9 +1,29 @@
+library(stringi)
+
 word_count <- function(input) {
   
-  # extract words
+  string <- tolower(input)
   
-  # count each word in string
+  # extract unique words, so that counting works without duplicates
+  string %>% 
+    stri_extract_all_words() %>% 
+    unlist() %>% 
+    unique() -> 
+    words
+  
+  # count each word in string by utilising word boundary reg-ex
+  counts <- c()
+  for (w in words) {
+    string %>% 
+      stri_count_regex(paste0(w, "\\b")) ->
+      count
+    
+    counts <- append(counts, count)
+  }
+  # [ ] purrr::map?
   
   # construct results list
-
+  names(counts) <- words
+  as.list(counts)
+  
 }
