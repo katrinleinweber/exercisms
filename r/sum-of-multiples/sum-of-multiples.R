5ed275d7da9dbb1ec3328eb9974eb5e844d3bb14
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-06-04 09:01:37 +0200

    solve naively with nested for loops
    http://exercism.io/submissions/0c19f3af107a465cb2a363985c65a536

--- r/sum-of-multiples/sum-of-multiples.R
+++ r/sum-of-multiples/sum-of-multiples.R
@@ -1,9 +1,16 @@
 sum_of_multiples <- function(factors, limit) {
   
   # create vector of candidate integers
+  candidates <- seq(limit - 1)
   
   # narrow candidates down to factors & multiples
+  multiples <- c()
+  for (f in factors)
+    for (c in candidates)
+      if (c %% f == 0)
+        multiples <- append(multiples, c)
   
   # sum those up
+  sum(unique(multiples))
   
 }
