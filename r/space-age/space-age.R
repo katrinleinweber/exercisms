4b2b4b0ce56602762d31f3ca3d482d98c18cd973
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-06-04 08:52:01 +0200

    iterate on conversion function
    http://exercism.io/submissions/925d3966a2664034a268b544dbb3e71b

--- r/space-age/space-age.R
+++ r/space-age/space-age.R
@@ -1,19 +1,18 @@
 space_age <- function(seconds, planet) {
   
-  Earth_seconds_per_year <- 31557600
+  # switch provided planet name with its relative orbital period
+  convert <- function(planet) {
+    switch(planet,
+           "mercury" = 0.2408467,
+           "venus"   = 0.61519726,
+           "earth"   = 1,
+           "mars"    = 1.8808158,
+           "jupiter" = 11.862615,
+           "saturn"  = 29.447498,
+           "uranus"  = 84.016846,
+           "neptune" = 164.79132)
+  }
   
-  # calculate factor on demand
-  conversion <- list(
-    mercury = Earth_seconds_per_year * 0.2408467,
-    venus = Earth_seconds_per_year * 0.61519726,
-    earth = Earth_seconds_per_year * 1,
-    mars = Earth_seconds_per_year * 1.8808158,
-    jupiter = Earth_seconds_per_year * 11.862615,
-    saturn = Earth_seconds_per_year * 29.447498,
-    uranus = Earth_seconds_per_year * 84.016846,
-    neptune = Earth_seconds_per_year * 164.79132
-  )
-  
-  # divide given age by planet-specific factor
-  round(seconds / conversion[[planet]], 2)
+  # divide given age by product of sec_per_Earth_year and relative orbital period
+  round(seconds / (31557600 * convert(planet)), 2)
 }
