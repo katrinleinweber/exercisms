07f736ab6eae59313d56515c70e5eb35ac56a998
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-06-06 20:40:27 +0200

    solve by trial & error in if-else predicates
    http://exercism.io/submissions/173f84b9d48d46ab8879de2c5976408b

--- r/phone-number/phone-number.R
+++ r/phone-number/phone-number.R
@@ -1,9 +1,25 @@
+library(magrittr)
+library(stringi)
+
 parse_phone_number <- function(number_string) {
   
-  # reject, if not starting with 1 or not 
-  
-  # find regex to remove any punctuation
-  
-  # reject if not exactly 10 digits long or 1 as 4th digit
-  
+  # proceed only, if string contains nothing other than 
+  # digits, dots, hyphens, parentheses and/or spaces
+  if (!stri_detect_regex(number_string, "[^0-9.\\-() ]")) {
+    
+    # remove valid non-digit characters
+    number_string %>%
+      stri_extract_all_regex("[0-9]") %>%
+      unlist() %>%
+      paste(collapse = "") ->
+      phone_number
+    
+    # return only if exactly 10 digits, or 11 and starting with 1
+    if (nchar(phone_number) == 10)
+      return(phone_number)
+    else if (nchar(phone_number) == 11 &
+             stri_detect_regex(phone_number, "^1"))
+      return(gsub("^1", "", phone_number))
+    # [ ] refactor to dplyr::case_when without "subscript out of bounds" error
+  }
 }
