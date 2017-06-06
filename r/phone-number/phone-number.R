f9fe00e3a7f7f97604aa9fd47f9fc1d58b39acc1
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-06-06 19:28:47 +0200

    start exercism.io/tracks/r/phone-number

---
+++ r/phone-number/phone-number.R
@@ -0,0 +1,9 @@
+parse_phone_number <- function(number_string) {
+  
+  # reject, if not starting with 1 or not 
+  
+  # find regex to remove any punctuation
+  
+  # reject if not exactly 10 digits long or 1 as 4th digit
+  
+}
