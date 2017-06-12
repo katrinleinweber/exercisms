5467ac1fb0f175f260d692284b63bff81f6a49a5
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-06-12 19:40:49 +0200

    solve naively with for-loop
    http://exercism.io/submissions/b31431833db449edbc2012367524cb31

--- r/rotational-cipher/rotational-cipher.R
+++ r/rotational-cipher/rotational-cipher.R
@@ -1,9 +1,31 @@
+library(dplyr)
+library(magrittr)
+
 rotate <- function(text, key) {
   
   # convert text to ASCII decimals
+  text %>%
+    utf8ToInt() ->
+    bytes
   
-  # add key to decimal
+  # separately for upper- and lowercase alphabet:
+  # 1. shift decimal range to 1
+  # 2. add key to each decimal
+  # 3. ensure wrap-around by modulus
+  # 4. reset range
+  for (b in 1:length(bytes)) {
+    if (between(bytes[b], 65, 90))
+      bytes[b] <- (bytes[b] - 65 + key) %% 26 + 65
+    else-if (between(bytes[b], 97, 122))
+      bytes[b] <- (bytes[b] - 97 + key) %% 26 + 97
+    else
+      bytes[b] <- bytes[b]
+  }
+  # [ ] b in bytes, without length?
+  # [ ] purr::map or ::walk?
   
   # convert back to (cipher)text
+  bytes %>% 
+    intToUtf8()
   
 }
