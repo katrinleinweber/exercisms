07e24f574ab991cb9ec763b41654d4bcb8c43932
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-06-13 18:54:32 +0200

    solve less naively
    http://exercism.io/submissions/cbd84f68364c41feb2b21b97fdbc036d

--- r/scrabble-score/scrabble-score.R
+++ r/scrabble-score/scrabble-score.R
@@ -1,9 +1,26 @@
+library(magrittr)
+
 scrabble_score <- function(input){
   
-  # split input into letters
-  
   # match of letter to score
+  score <- function(letter) {
+    dplyr::case_when(
+      letter %in% c("A", "E", "I", "O", "U", "L", "N", "R", "S", "T") ~ 1,
+      letter %in% c("D", "G") ~ 2,
+      letter %in% c("B", "C", "M", "P") ~ 3,
+      letter %in% c("F", "H", "V", "W", "Y") ~ 4,
+      letter %in% c("K") ~ 5,
+      letter %in% c("J", "X") ~ 8,
+      letter %in% c("Q", "Z") ~ 10
+    )}
   
-  # sum scores ups
+  # split input into letters
+  # sum scores up
+  input %>% 
+    toupper() %>% 
+    strsplit("") %>% 
+    purrr::map(.f = score) %>% 
+    unlist() %>% 
+    sum()
   
 }
