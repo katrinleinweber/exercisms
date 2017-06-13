7dd3c777b7b60eb55d5d1048d8746b5eed6f9a28
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-06-13 19:01:34 +0200

    iterate on performance: sort cases by most-to-least
    microbenchmark(scrabble_score("abcdefghijklmnopqrstuvwxyz") shows a few ms faster execution in median, than with `case_when()` sorted from least-to-most cases. Many ms faster when looking at mean & max, though.

--- r/scrabble-score/scrabble-score.R
+++ r/scrabble-score/scrabble-score.R
@@ -6,12 +6,12 @@ scrabble_score <- function(input){
   score <- function(letter) {
     dplyr::case_when(
       letter %in% c("A", "E", "I", "O", "U", "L", "N", "R", "S", "T") ~ 1,
-      letter %in% c("D", "G") ~ 2,
-      letter %in% c("B", "C", "M", "P") ~ 3,
       letter %in% c("F", "H", "V", "W", "Y") ~ 4,
-      letter %in% c("K") ~ 5,
+      letter %in% c("B", "C", "M", "P") ~ 3,
+      letter %in% c("D", "G") ~ 2,
       letter %in% c("J", "X") ~ 8,
-      letter %in% c("Q", "Z") ~ 10
+      letter %in% c("Q", "Z") ~ 10,
+      letter %in% c("K") ~ 5
     )}
   
   # split input into letters
