753da643166d593289fb30a7171f2461fd9abcc4
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-06-14 18:38:17 +0200

    solve with stringi
    http://exercism.io/submissions/a32636d4830841079a56d8f51fff9299

--- r/isogram/isogram.R
+++ r/isogram/isogram.R
@@ -1,5 +1,15 @@
+library(magrittr)
+
 is_isogram <- function(word) {
   
+  word %>%
+    stringi::stri_extract_all_regex("\\w") %>%
+    unlist %>%
+    tolower ->
+    letters
+  
   # compare length of letter vectors before & after unique()
+  identical(length(letters),
+            length(unique(letters)))
   
 }
