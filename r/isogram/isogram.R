6e9c9ed535810679129bf7677d2b487d0a8d2780
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-06-14 18:46:40 +0200

    iterate on function calls (on intermediate or input)
    http://exercism.io/submissions/4b53b25fa9c7431f9fe682eaea21c452

--- r/isogram/isogram.R
+++ r/isogram/isogram.R
@@ -3,11 +3,12 @@ library(magrittr)
 is_isogram <- function(word) {
   
     gsub(pattern = "\\W", replacement = "", x = word) %>%
+    strsplit("") %>% 
+    unlist %>% 
     tolower ->
     letters
   
-  # compare length of letter vectors before & after unique()
-  identical(length(letters),
-            length(unique(letters)))
+  # compare letter vector before & after unique()
+  identical(letters, unique(letters))
   
 }
