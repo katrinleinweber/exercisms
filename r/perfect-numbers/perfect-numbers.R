9eaa7ffde725bc0d009bf503c075088ca450e764
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-06-21 19:45:52 +0200

    solve naively and with unacceptably high test runtime
    http://exercism.io/submissions/1958bc83349c430c97d0b133445cf567

--- r/perfect-numbers/perfect-numbers.R
+++ r/perfect-numbers/perfect-numbers.R
@@ -1,7 +1,29 @@
+library(magrittr)
+
 is_perfect <- function(n){
   
+  # catch edge cases
+  if (n <= 0)
+    stop("Only natural number can be classified here!")
+  
+  if (n == 1)
+    return("deficient")
+  
+  
+  aliquots <- c()
+  
   # find n's factors, incl. 1 but not n (aliquots)
-  
+  for (i in seq(n - 1)) {
+    if (n %% i == 0)
+      aliquots <- append(aliquots, i)
+  }
+    
   # calculate sum and classify n
+  sum <- as.numeric(sum(aliquots))
   
+  dplyr::case_when(
+    sum == n ~ "perfect",
+    sum < n ~ "deficient",
+    sum > n ~ "abundant"
+  )
 }
