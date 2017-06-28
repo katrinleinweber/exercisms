f1edb46553e1f99cb70420c753a15bbd91bb412a
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-06-28 19:44:39 +0200

    solve with base functions
    http://exercism.io/submissions/dcbbf1d0d34543efb78075d5133f1467

--- r/prime-factors/prime-factors.R
+++ r/prime-factors/prime-factors.R
@@ -1,7 +1,19 @@
 prime_factors <- function(number) {
 
   # set start conditions
+  factors <- c()
+  f <- 2
   
   # loop through checking factors for even division and append
+  # [ ] recursion possible as in collatz.R?
+  while (number > 1) {
+    if (number %% f == 0) {
+      factors <- c(factors, f)
+      number <- number / f
+    } else {
+      f <- f + 1
+    }
+  }
   
+  factors
 }
