aba200aa8589377c62767f813d4c65100005ca0c
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-07-01 10:34:56 +0200

    solve naively, with warning in L10
    http://exercism.io/submissions/2cf7a3ff932e4774933f7dbe863e35ed

--- r/largest-series-product/largest-series-product.R
+++ r/largest-series-product/largest-series-product.R
@@ -1,11 +1,32 @@
-largest_series_product <- function(digits, span){
+library(magrittr)
 
-  # turn digit string into integer vector
+largest_series_product <- function(digits, span) {
   
-  # extract spanned digits, then loop or functionalise
+  # catch edge cases & invalid input
+  if (nchar(digits) < span)
+    stop("Span must not be longer then the digit string!")
+  else if (digits == "" | span == 0)
+    return(1)
+  else if (is.na(as.numeric(digits)))
+    stop("There are non-digits in the digit string!")
+  
+  # turn digit string into integer vector
+  list <- digits %>%
+    strsplit("") %>%
+    unlist %>%
+    as.numeric
   
   # calculate number of steps
+  steps <- length(list) - span + 1
   
+  products <- c()
   # walk along vector
-
+  for (s in 1:steps) {
+    products <- c(products,
+                  prod(list[s:(s + span - 1)]))
+  }
+  
+  max(products)
+  
+  # [ ] speed up by ignoring steps with 0
 }
