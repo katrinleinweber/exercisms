962966b99e5be32593dc24296d2d1cade7e41601
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-07-01 09:54:29 +0200

    start exercism.io/tracks/r/exercises/largest-series-product

---
+++ r/largest-series-product/largest-series-product.R
@@ -0,0 +1,11 @@
+largest_series_product <- function(digits, span){
+
+  # turn digit string into integer vector
+  
+  # extract spanned digits, then loop or functionalise
+  
+  # calculate number of steps
+  
+  # walk along vector
+
+}
