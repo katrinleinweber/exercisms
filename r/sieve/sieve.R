94748d8887968d396a54877eb2fb7daf00e55a75
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-07-01 07:00:30 +0200

    solve naively
    http://exercism.io/submissions/16c877c6857a4b928897e96e959baa3c

--- r/sieve/sieve.R
+++ r/sieve/sieve.R
@@ -1,10 +1,17 @@
 sieve <- function(limit) {
   
-  # catch edge cases
+  # catch edge case
+  if (limit == 1)
+    return()
   
   # list candidates
+  candidates <- 2:limit
   
   # walk through list, calculate multiples and eliminate
+  for (c in candidates) {
+    multiples <- candidates * c
+    candidates <- setdiff(candidates, multiples)
+  }
   
+  candidates
 }
-
