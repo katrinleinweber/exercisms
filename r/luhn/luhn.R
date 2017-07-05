66c7721a5c73b4790aa690112b194be52bd843fc
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-07-05 19:46:06 +0200

    iterate on elegance
    http://exercism.io/submissions/615d931ecc9240bf8d69e34741f12987

--- r/luhn/luhn.R
+++ r/luhn/luhn.R
@@ -18,20 +18,16 @@ is_valid <- function(input) {
     as.integer %>%
     rev
   
-  # conduct Luhn's algorithm
-  # [ ] more elegant
-  for (d in seq(1, length(stigid)-1, 2)) {
-    d <- d+1
+  # conduct Luhn's algorithm: double every second digits, but stay below 10
+  for (d in seq(2, length(stigid), 2)) {
     stigid[d] <- 2 * stigid[d]
     if (stigid[d] > 9)
       stigid[d] <- stigid[d] - 9
   }
   
   # conforms to Luhn, if divisible by 10
-  # [ ] only revert & convert once
-  if (stigid %>% rev %>% as.integer %>% sum %% 10 == 0)
+  if (stigid %>% sum %% 10 == 0)
     return(TRUE)
   else
     return(FALSE)
 }
-
