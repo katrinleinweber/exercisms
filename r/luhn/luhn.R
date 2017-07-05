9b691bbec3e9a1e11331534337ae3eb2f186e144
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-07-05 19:39:36 +0200

    solve naively
    http://exercism.io/submissions/e6c715437ead4290b28a2b6b204e850d

--- r/luhn/luhn.R
+++ r/luhn/luhn.R
@@ -1,13 +1,41 @@
-# Determine whether the number is valid.
+library(magrittr)
+
 is_valid <- function(input) {
   
-  # catch edge cases & invalid input
+  # convert to digits
+  digits <- input %>%
+    gsub(" ", "", x = .) %>%
+    strsplit("") %>%
+    unlist
   
-  # convert to number
+  # reject too small input
+  if (length(digits) <= 1)
+    return(FALSE)
   
-  # apply Luhn's algorithm
+  # reject non-digit characters
+  if (TRUE %in% stringi::stri_detect_regex(digits, "\\D"))
+    return(FALSE)
+  # [ ] compound all checks
   
-  # check validity
-
+  # prepare for Luhn's algorithm
+  stigid <- digits %>%
+    as.integer %>%
+    rev
+  
+  # conduct Luhn's algorithm
+  # [ ] more elegant
+  for (d in seq(1, length(stigid)-1, 2)) {
+    d <- d+1
+    stigid[d] <- 2 * stigid[d]
+    if (stigid[d] > 9)
+      stigid[d] <- stigid[d] - 9
+  }
+  
+  # conforms to Luhn, if divisible by 10
+  # [ ] only revert & convert once
+  if (stigid %>% rev %>% as.integer %>% sum %% 10 == 0)
+    return(TRUE)
+  else
+    return(FALSE)
 }
 
