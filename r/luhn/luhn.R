eb086ee7e0c15e95dc6a6ed9356dc0deea7a69b8
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-07-05 19:37:12 +0200

    start exercism.io/tracks/r/exercises/luhn

---
+++ r/luhn/luhn.R
@@ -0,0 +1,13 @@
+# Determine whether the number is valid.
+is_valid <- function(input) {
+  
+  # catch edge cases & invalid input
+  
+  # convert to number
+  
+  # apply Luhn's algorithm
+  
+  # check validity
+
+}
+
