7f77ab1ab1374a50587fe22dca5b2923a4716ab5
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-08-25 17:14:17 +0200

    solve naively
    http://exercism.io/submissions/62b31188d2694f25b31863df20e1301f

--- r/allergies/allergies.R
+++ r/allergies/allergies.R
@@ -1,18 +1,34 @@
 allergy <- function(num) {
   
   # convert score to binary
+  binary <- rawToChar(intToBits(num), multiple = TRUE)[1:8]
   
   # match places with 1
   # construct list
+  
+  allergies <- character()
+  
+  if (binary[1] != "") allergies <- append(allergies, "eggs")
+  if (binary[2] != "") allergies <- append(allergies, "peanuts")
+  if (binary[3] != "") allergies <- append(allergies, "shellfish")
+  if (binary[4] != "") allergies <- append(allergies, "strawberries")
+  if (binary[5] != "") allergies <- append(allergies, "tomatoes")
+  if (binary[6] != "") allergies <- append(allergies, "chocolate")
+  if (binary[7] != "") allergies <- append(allergies, "pollen")
+  if (binary[8] != "") allergies <- append(allergies, "cats")
+
+  return(allergies)
 }
 
 allergic_to <- function(allergy_object, allergy) {
   
   # check element in list
+  allergy %in% allergy_object
 }
 
 list_allergies <- function(allergy_object) {
   
   # pass list through
+  return(allergy_object)
 }
 
