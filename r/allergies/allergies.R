2c9a7ba9616de0fc2c913f401d2f262398feb421
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-08-25 17:04:00 +0200

    start exercism.io/tracks/r/exercises/allergies

---
+++ r/allergies/allergies.R
@@ -0,0 +1,18 @@
+allergy <- function(num) {
+  
+  # convert score to binary
+  
+  # match places with 1
+  # construct list
+}
+
+allergic_to <- function(allergy_object, allergy) {
+  
+  # check element in list
+}
+
+list_allergies <- function(allergy_object) {
+  
+  # pass list through
+}
+
