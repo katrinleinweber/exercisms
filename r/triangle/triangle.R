1fae8c60c6efa788c8639b818c7769ece942ccde
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-08-29 16:29:44 +0200

    solve with pipe
    http://exercism.io/exercises/7fbe6d72ac5b4f548c102ee4bc4009f1

--- r/triangle/triangle.R
+++ r/triangle/triangle.R
@@ -1,7 +1,21 @@
+library(magrittr)
+
 triangle <- function(x, y, z) {
   
   # exclude invalid cases
+  if (x == 0
+      | y == 0
+      | z == 0
+      | x + y < z
+      | z + x < y
+      | y + z < x
+  ) stop("That's not really a triangle!")
   
   # construct S3 class
+  c(x, y, z) %>% 
+    unique() %>% 
+    length() %>% 
+    switch(c("equilateral", "isosceles"), "isosceles", "scalene") %>% 
+    structure(c(x, y, z), class = .)
 }
 
