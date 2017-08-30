7c84e59e6aa2d7937d26ce96c3af316783904630
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-08-30 08:05:02 +0200

    solve with pipe

--- r/pangram/pangram.R
+++ r/pangram/pangram.R
@@ -1,4 +1,14 @@
+library(magrittr)
+
 is_pangram <- function(input) {
 
   # reduce char space, sort and compare against letter list
+  input %>% 
+    tolower() %>% 
+    strsplit("") %>% 
+    unlist() %>% 
+    unique() %>% 
+    grep(pattern = "[a-z]", value = TRUE) %>% 
+    sort() %>% 
+    identical(letters)
 }
