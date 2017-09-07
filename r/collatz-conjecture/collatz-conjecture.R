defc1eff981d3317061635c9d7bcf86a83bca875
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-09-07 21:12:26 +0200

    solve
    http://exercism.io/submissions/9ef6d7ffe1464e3e8708eec51e933930

--- r/collatz-conjecture/collatz-conjecture.R
+++ r/collatz-conjecture/collatz-conjecture.R
@@ -1,6 +1,15 @@
 collatz_step_counter <- function(num) {
   
   # initialise counter
+  steps <- 0
   
   # recurse through cases, after testing for base case
+  if (num == 1)
+    steps
+  else if (num %% 2 == 0)
+    1 + collatz_step_counter(num / 2)
+  else if (num %% 2 != 0)
+    1 + collatz_step_counter(num * 3 + 1)
+  else  # if (num <= 0)
+    stop("Num must be an integer larger than 0.")
 }
