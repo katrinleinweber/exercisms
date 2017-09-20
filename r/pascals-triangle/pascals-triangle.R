7715a2a8856b41e582dbec9595328d307c8dff16
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-09-20 18:22:45 +0200

    solve naively
    http://exercism.io/submissions/e5742025ed6640cfaf4a22819b864e3a

--- r/pascals-triangle/pascals-triangle.R
+++ r/pascals-triangle/pascals-triangle.R
@@ -1,9 +1,28 @@
 pascals_triangle <- function(n) {
   
   # catch invalid input & edge cases 
-  
+  if (n < 0 | is.null(n))
+    stop("Invalid! Please provide n > 0!")
+  else if (n == 0)
+    list()
+  else if (n == 1)
+    list(1)
+  else if (n == 2)
+    list(1, c(1, 1))
+  else if (n >= 3) {
+    
   # construct triangle
-  
+    t <- list(1, c(1, 1))
+    
+    for (i in seq(3, n)) {
+      t <- append(t, list(c(i)))
+      t[[i]][1] <- 1
+      t[[i]][i] <- 1
+      
   # construct row
-  
+      for (j in seq(2, i-1))
+        t[[i]][j] <- t[[i-1]][j-1] + t[[i-1]][j]
+    }
+    return(t)
+  }
 }
