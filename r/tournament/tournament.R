c252840ec6b40cb8135c55fd34b6bb5ac248a097
Katrin Leinweber <katrin.leinweber@uni-konstanz.de>
2017-09-26 17:48:16 +0200

    solve with too many if-else & for-loop constructs
    http://exercism.io/submissions/6ac00291f4d64c6090b4e3f1db323489

--- r/tournament/tournament.R
+++ r/tournament/tournament.R
@@ -1,11 +1,57 @@
+library(dplyr)
+library(magrittr)
+
 tournament <- function(input) {
 
-  # clean input data
+  # clean input data (oust empty rows)
+  input1 <- input[lapply(input, nchar) > 0]
   
   # parse input into data frame
+  input1 %>% 
+    stringi::stri_extract_all(regex = "[^;]+") ->  # greedy everything except ;
+    input2
   
-  # calculate values
+  # oust lines with incorrect number of cells
+  input3 <- input2[lapply(input2, length) == 3]
   
-  # output data frame
+  df <- data.frame()
+  for (r in 1:length(input3))
+    for (c in 1:length(input3[[r]]))
+      df[r, c] <- input3[[r]][c]
+  names(df) <- c("Team1", "Team2", "Result")
   
+  # separate team pairs, marking invalid result strings
+  Result2 <- list()
+  for (r in 1:length(df$Result)) {
+    if (df$Result[r] == "win") Result2[r] <- "loss"
+    else if (df$Result[r] == "loss") Result2[r] <- "win"
+    else if (df$Result[r] == "draw") Result2[r] <- "draw"
+    else if (df$Result[r] != "draw") Result2[r] <- "NA"
+  }
+  
+  Result2 %>% 
+    unlist() %>% 
+    data.frame(df$Team2, .) -> 
+    df2
+  names(df2) <- c("Team", "Result")
+  
+  # oust rows with invalid results/cells
+  df2 <- df2[df2$Result != "NA",]
+  
+  df$Team2 <- NULL
+  names(df) <- c("Team", "Result")
+  df <- rbind(df, df2)
+  
+  # calculate values & output data frame
+  df %>% 
+    mutate(W = ifelse(Result == "win", 1, 0),
+           D = ifelse(Result == "draw", 1, 0),
+           L = ifelse(Result == "loss", 1, 0),
+           P = 3 * W + D) %>% 
+    select(-Result) %>% 
+    group_by(Team) %>% 
+    summarise_all(sum) %>% 
+    mutate(MP = W + D + L) %>% 
+    select(Team, MP, W:P) %>% 
+    arrange(desc(P))
 }
